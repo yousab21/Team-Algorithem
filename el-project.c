43bf1efb3815d40f579aaa05403da68c1d59a8cb
yousab21 <yousabnasrala@gmail.com>
2025-04-25 18:21:32 +0300

    feature : copouns

--- el-project.c
+++ el-project.c
@@ -16,6 +16,9 @@ typedef struct item {
 
 double full_price = 0;
 
+    char coupons[4][10] = { "yousab", "yahia", "mohamed", "yassin" };
+    float discount[4] = { 0.1, 0.2, 0.3, 0.4 };
+
 // Overnight shipping prompt
 int overnight_checker() {
     char a;
@@ -121,7 +124,28 @@ int main() {
     // Overnight shipping option
     bool overnight = overnight_checker();
 
-    // Final receipt
+    char coupon_response ,coupon_code[20];
+    int coupon_index = -1;
+    float discount_value = 0.0;
+
+    printf("do you have a discount coupon [y/n] :");
+    scanf(" %c", &coupon_response);
+    if (coupon_response == 'y' || coupon_response == 'Y') {
+      
+        printf("Please enter your coupon code: ");
+        scanf("%s", coupon_code);
+           for (coupon_index = 0; coupon_index < 4; coupon_index++) {
+            if (strcmp(coupons[coupon_index], coupon_code) == 0) {
+                discount_value = full_price * discount[coupon_index];
+                break;
+            }
+        }
+    }
+    else {
+        printf("invalid copoun \n");
+    }
+    
+
     printf("\n\t\tYour Receipt:\n\n");
     for (int i = 0; i < items_number; i++) {
         printf("%s\t\tPrice: %.2f$\n", item[i].name, item[i].price);
@@ -131,8 +155,10 @@ int main() {
     if (overnight) {
         printf("Overnight delivery:\t+5.00$\n");
     }
-
-    printf("TOTAL PRICE:\t\t%.2lf$\n", full_price);
+    printf("raw price:\t\t%.2lf$\n", full_price);
+    printf("Discount:\t\t-%.2f$\n", discount_value);
+    full_price -= discount_value;
+    printf("Final price:\t\t%.2lf$\n", full_price);
     printf("\nThanks For Buying From Us!\nSee you soon :)\n");
 
     return 0;
