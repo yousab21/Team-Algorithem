ca42e89597d7ee976369ab98dfeae9a2ec2b5f38
yousab21 <yousabnasrala@gmail.com>
2025-04-25 13:35:09 +0300

    nadaft el code = bug fixes

--- el-project.c
+++ el-project.c
@@ -1,150 +1,139 @@
-//yousab nabil nasrala || section (39)
-//Yahia Khaled Elsafty || Section (39)
-//Mohamed Hamada AboSeda || Secton (39)
-//Yassin Khaled Elnaggar || section (39)
+// Yousab Nabil Nasrala || Section (39)
+// Yahia Khaled Elsafty || Section (39)
+// Mohamed Hamada AboSeda || Section (39)
+// Yassin Khaled Elnaggar || Section (39)
 
 #include <stdio.h>
-#include <windows.h>
+#include <unistd.h>
 #include <stdbool.h>
-//struct of item
-typedef struct item
-{
+#include <string.h>
+
+// Struct of item
+typedef struct item {
     char name[30];
     float price;
-}data;
+} data;
 
-double full_price=0;
+double full_price = 0;
 
-//overnight shipping asking
-int overnight_checker()
-{
+// Overnight shipping prompt
+int overnight_checker() {
+    char a;
+    while (1) {
+        printf("Overnight shipping? [y/n]: ");
+        scanf(" %c", &a);  // Note the space before %c to skip leftover newline
 
-    for (;;)
-    {
-        char a;
-        printf("overnight shiping?[y/n] ");
-        scanf("%c",&a);
-        if (a == 'y' || a == 'Y')
-        {
-            full_price+=5;
+        if (a == 'y' || a == 'Y') {
+            full_price += 5;
             return 1;
-        }
-        else if (a == 'n'|| a == 'N')
-        {
-            full_price+=0;
+        } else if (a == 'n' || a == 'N') {
             return 0;
+        } else {
+            printf("Wrong input! Please try again.\n");
         }
-        else 
-       printf("wrong input! Please try again\n");
     }
 }
 
-int main()
-{
-
-    //some var
-    int items_number, temp ,counter=1 ,i= 0,n=0,total_shipping=0;
-    bool run=1,overnight_checker1;
-    data item[items_number];
+int main() {
+    int items_number = 0, total_items = 0, counter = 1;
+    int total_shipping = 0;
+    bool run = true;
+    data item[100];  // Set a max of 100 items
 
     printf("Welcome to our small shop\n\n");
-    while(run){
-        //num of items 
-        if (counter == 1)
-        {
-            printf ("Please enter the number of items you want ot buy (minimum 2): ");
-            scanf("%d",&items_number);
-            fflush(stdin);
 
-            if (items_number<2)
-            {
-                 printf("wrong input");
-                return 1;
+    while (run) {
+        int new_items = 0;
+
+        if (counter == 1) {
+            printf("Please enter the number of items you want to buy (minimum 2): ");
+        } else {
+            printf("If you clicked YES by mistake, you can write 0 to stop.\n");
+            printf("Please enter the number of items you want to add: ");
+        }
+
+        while (scanf("%d", &new_items) != 1 || new_items < 0) {
+            printf("Invalid input. Enter a non-negative number: ");
+            while (getchar() != '\n');
+        }
+
+        if (counter == 1 && new_items < 2) {
+            printf("Wrong input. You must buy at least 2 items.\n");
+            return 1;
+        }
+
+        if (new_items == 0 && counter != 1) {
+            break; // stop adding more items
+        }
+
+        // Store total items
+        int start_index = items_number;
+        items_number += new_items;
+
+        // Collect item data
+        printf("\nTHANKS! Now enter the specifications of your items:\n\n");
+        for (int i = start_index; i < items_number; i++, counter++) {
+            while (getchar() != '\n'); // clear input buffer
+
+            printf("Please enter the name of item %d: ", counter);
+            fgets(item[i].name, sizeof(item[i].name), stdin);
+            strtok(item[i].name, "\n"); // remove newline
+
+            printf("Enter the price of item %d: ", counter);
+            while (scanf("%f", &item[i].price) != 1 || item[i].price < 0) {
+                printf("Invalid input. Please enter a positive number: ");
+                while (getchar() != '\n');
             }
-        }
-        else
-        {
-            printf ("if you have mistakenly clicked YES you can write 0\n");
-            printf ("Please enter the number of items you want ot buy: ");
 
-
-            scanf("%d",&items_number);
-            fflush(stdin);
-            items_number+=temp;
-        }
-
-
-
-        printf("THANKS! Now enter the specefies of your items\n");
-
-// data of the items
-        for (; i< items_number;)
-        {
-            printf("Please enter the name of your %d item: \n",counter);
-            fgets(item[i].name , 30 , stdin);
-
-            printf("Enter the price of your %d item: \n",counter);
-            scanf("%f", &item[i].price);
-
-            fflush(stdin);
             full_price += item[i].price;
 
-            i++;
-            counter++;
-        }
-
-        //shipping
-        for(; n<items_number;n++ )
-        {
-            if(item[n].price<10)
-            {
+            // Shipping cost per item
+            if (item[i].price < 10) {
                 full_price += 2;
-                total_shipping +=2;
-            }
-            else
-            {
-                full_price +=3;
-                total_shipping +=3;
+                total_shipping += 2;
+            } else {
+                full_price += 3;
+                total_shipping += 3;
             }
         }
 
+        printf("\nCurrent total price (including shipping): %.2lf$\n", full_price);
+        printf("Current total shipping cost: %d$\n", total_shipping);
+        sleep(1);
 
+        // Ask if the user wants to continue
+        char response;
+        while (1) {
+            printf("\nDo you want to continue shopping? [y/n]: ");
+            scanf(" %c", &response);
 
-        //show full price before Receipt 
-        printf("your full price now is :%0.2lf\n",full_price);
-        printf("your full price now is :%d\n",total_shipping);
-
-        Sleep(3000);
-        //cheking if you want to continue 
-        //if the user Entered yes by mistake he could easily write 0 in new shopping quistions
-        int continue1 = MessageBox(NULL,"Do you want to continue?","continue shopping",MB_YESNO |MB_ICONQUESTION|MB_DEFBUTTON1|MB_TASKMODAL);
-        if (continue1 ==6)
-        {
-            temp = items_number;
-            run = 1;
-        }
-        else
-            {
-            run =0;
+            if (response == 'y' || response == 'Y') {
+                break;  // continue loop
+            } else if (response == 'n' || response == 'N') {
+                run = false;
+                break;
+            } else {
+                printf("Invalid input. Please type 'y' or 'n'.\n");
             }
+        }
     }
 
-    //overnight 
-    overnight_checker1=overnight_checker();
+    // Overnight shipping option
+    bool overnight = overnight_checker();
 
-    //Receipt
-    printf("\t\tYour Receipt:\n");
-
-    for(int m=0;m<items_number;)
-    {
-        printf("%s\tPrice +%0.2f$\n",item[m].name,item[m].price);
-        m++;
+    // Final receipt
+    printf("\n\t\tYour Receipt:\n\n");
+    for (int i = 0; i < items_number; i++) {
+        printf("%s\t\tPrice: %.2f$\n", item[i].name, item[i].price);
     }
 
-    printf("Total shipping \n\t price +%d$\n",total_shipping);
-    if(overnight_checker1==1)
-        printf("Overnight delivering\n\tprice +5.00$\n");
+    printf("\nTotal shipping:\t\t+%d$\n", total_shipping);
+    if (overnight) {
+        printf("Overnight delivery:\t+5.00$\n");
+    }
 
-    printf("TOTAL PRICE =\t %0.2lf\n",full_price);
-    printf("Thanks For Buying From US!!\nSEE YOU SOON");
+    printf("TOTAL PRICE:\t\t%.2lf$\n", full_price);
+    printf("\nThanks For Buying From Us!\nSee you soon :)\n");
+
+    return 0;
 }
